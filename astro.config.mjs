1b371b5b3d25defdfad1414e668cff44caec5791
Joel Buenrostro <joelbuenrostro@outlook.com>
2024-10-13 00:00:23 -0600

    Y20 - Componente de pie de pagina
    Creacion del componente social para el pie de pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import {loadEnv} from "vite";
 
+import react from "@astrojs/react";
+
 const {IMAGE_DOMAIN} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
@@ -9,9 +11,7 @@ export default defineConfig({
   image: {
     domains: [IMAGE_DOMAIN],
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react()],
 });
